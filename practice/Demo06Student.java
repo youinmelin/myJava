cbb4a4a7e7d9d0386e71b180387d73451c7564cd
youinmelin <ygqs@sina.com>
2020-04-17 12:57:27 +0800

    new

---
+++ practice/Demo06Student.java
@@ -0,0 +1,8 @@
+public class Demo06Student {
+    public static void main (String[] args){
+        Student stu1 = new Student();
+        stu1.name = "xiao ming";
+        System.out.println(stu1.name);
+        stu1.study();
+    }
+}
---
+++ practice/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int age;
+
+    public void eat() {
+        System.out.println("eating");
+    }
+
+    public void study(){
+        System.out.println("studying");
+    }
+}
+
