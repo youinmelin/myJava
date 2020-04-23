2714c791ce94cbf8424b427b3dd141587ce88995
youinme <ygqs@sina.com>
2020-04-23 11:39:49 +0800

    new

---
+++ practice/demo13Interface/Interface07Private.java
@@ -0,0 +1,10 @@
+package demo13Interface;
+public interface Interface07Private {	
+	default void methodDefault(){
+		System.out.println("This is a default method.");
+		commonMethod();
+	}
+	private void commonMethod(){
+		System.out.println("This is a common method.");
+	}
+}
