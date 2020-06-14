d8987360e2ebf3d090b23af9ac981dc8325295c8
youinmelin <ygqs@sina.com>
2020-06-14 09:02:53 +0800

    new

---
+++ practice/demo13Interface/MyInterface07.java
@@ -0,0 +1,8 @@
+package src.demo13Interface;
+public class MyInterface07 {
+	public static void main(String[] args) {
+		Interface07PrivateImpl inter = new Interface07PrivateImpl(){
+			inter.
+			
+	}
+}
