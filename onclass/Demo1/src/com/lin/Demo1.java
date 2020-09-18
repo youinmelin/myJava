3a077a8a001488f98bc25b3b360dacedc404c7b3
youinmelin <ygqs@sina.com>
2020-09-18 16:51:44 +0800

    spring boot

--- onclass/Demo1/src/com/lin/Demo1.java
+++ onclass/Demo1/src/com/lin/Demo1.java
@@ -1,16 +1,20 @@
 package com.lin;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Demo1 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.print("hello java");
+		System.out.print("请输入自动关机时间（秒）：");
+		Scanner input = new Scanner(System.in);
+		String shutdown_time = input.nextLine();
 		// shutdown pc
-//		Runtime.getRuntime().exec("shutdown -s -t 1000");
+		Runtime.getRuntime().exec("shutdown -s -t " + input);
 		// cancel shutdown
-		Runtime.getRuntime().exec("shutdown -a");
+//		Runtime.getRuntime().exec("shutdown -a");
 	}
 
 }
