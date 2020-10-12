5703a54bf0527a64059951bec4d2ff340ecffe01
youinmelin <ygqs@sina.com>
2020-10-12 22:32:04 +0800

    eureka

--- practice/demo58springcloud04/consumer_demo/src/main/java/com/lin/ConsumerApplication.java
+++ practice/demo58springcloud04/consumer_demo/src/main/java/com/lin/ConsumerApplication.java
@@ -2,12 +2,13 @@ package com.lin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan()
+@EnableDiscoveryClient
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class);
--- practice/demo58springcloud04/consumer_demo/src/main/java/com/lin/controller/ConsumerController.java
+++ practice/demo58springcloud04/consumer_demo/src/main/java/com/lin/controller/ConsumerController.java
@@ -2,20 +2,31 @@ package com.lin.controller;
 
 import com.lin.pojo.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/byid/{id}")
-    public UserEntity findById(Long id) {
-        String url = "http://localhost:9091/user/byid/1";
+    public UserEntity findById(@PathVariable Long id) {
+//        String url = "http://localhost:9091/user/byid/" + id;
+        // 获取eureka中注册的user-service的实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user_service");
+        ServiceInstance serviceInstance = instances.get(0);
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/byid/" + id;
         UserEntity userEntity = restTemplate.getForObject(url, UserEntity.class);
         return userEntity;
 
